406b34c75dbd206c087fa9b85595cf5d503d6d2c
Surendran Mahendran <surenspost@gmail.com>
2014-06-01 12:56:21 +0530

    Setting up stuff for local development

--- run.rb
+++ run.rb
@@ -2,6 +2,16 @@ require 'sinatra'
 
 set :bind, '0.0.0.0'
 set :port, '8000'
+set :protection, :except => :frame_options
+
+after do
+  headers({ 'X-Frame-Options' => 'ALLOW-FROM *' })
+end
+
+
+get "/" do
+  send_file "index.html"
+end
 
 get "/*" do |file|
   send_file file
